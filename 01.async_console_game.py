a0985519b0706975791c7bca20213589eda5de19
derevenets_artem <artem@comvoy.be>
2019-04-05 21:43:09 +0300

    01: Finish 10th step

--- 01.async_console_game.py
+++ 01.async_console_game.py
@@ -4,6 +4,36 @@ import random
 import time
 
 
+async def fire(canvas, start_row, start_column, rows_speed=0, columns_speed=1):
+    """Display animation of gun shot. Direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 async def blink(canvas, row, column, symbol):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
@@ -30,23 +60,32 @@ def draw(canvas):
     canvas.border()
     canvas.refresh()
 
-    symbols = "+*.:"
-    full_height, full_width = canvas.getmaxyx()
-
+    canvas_height, canvas_width = canvas.getmaxyx()
     number_of_stars = random.randint(50, 60)
 
-    coroutines = []
+    # Add initial action (coroutine) - shot out of a cannon
+    coroutine_fire = fire(canvas, canvas_height // 2, canvas_width // 2)
+    coroutines = [coroutine_fire]
+
+    # Form list of coroutines (1 coroutine - 1 star)
     for i in range(number_of_stars):
-        row = random.randint(1, full_height)
-        column = random.randint(1, full_width)
-        symbol = random.choice(symbols)
+        # Reducing max dimensions by 2 allows to avoid "curses.error"
+        row = random.randint(1, canvas_height - 2)
+        column = random.randint(1, canvas_width - 2)
+
+        symbol = random.choice("+*.:")
 
         coroutine = blink(canvas, row, column, symbol)
         coroutines.append(coroutine)
 
     while True:
         for coroutine in coroutines:
-            coroutine.send(None)
+            try:
+                coroutine.send(None)
+            # 'coroutine_fire' could raise StopIteration when reach the borders
+            except StopIteration:
+                coroutines.remove(coroutine)
+
         time.sleep(0.1)
         canvas.refresh()
 
