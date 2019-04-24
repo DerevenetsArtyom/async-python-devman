d6077d2ab0bdbd8a7e5faf0699227ee02ea8e4f0
derevenets_artem <artem@comvoy.be>
2019-04-24 19:10:35 +0300

    02: Rename 'files' folder to 'animation_frames'

--- 02.through_garbage_to_stars/read_frames.py
+++ 02.through_garbage_to_stars/read_frames.py
@@ -1,23 +1,23 @@
 def read_rocket_frames():
-    with open("files/rocket_frame_1.txt") as f:
+    with open("animation_frames/rocket_frame_1.txt") as f:
         frame1 = f.read()
 
-    with open("files/rocket_frame_2.txt") as f:
+    with open("animation_frames/rocket_frame_2.txt") as f:
         frame2 = f.read()
 
     return frame1, frame2
 
 
 def read_garbage_frames():
-    with open('files/trash_small.txt') as f:
+    with open('animation_frames/trash_small.txt') as f:
         small_garbage_frame = f.read()
 
-    with open('files/trash_large.txt') as f:
+    with open('animation_frames/trash_large.txt') as f:
         large_garbage_frame = f.read()
 
     return small_garbage_frame, large_garbage_frame
 
 
 def read_game_over_frame():
-    with open('files/game_over.txt') as f:
+    with open('animation_frames/game_over.txt') as f:
         return f.read()
