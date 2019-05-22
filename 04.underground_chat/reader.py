17aa1bb5ebe355ff00470e128415db0401ec0c74
derevenets_artem <artem@comvoy.be>
2019-05-22 20:39:51 +0300

    04: Add 'main' and 'get_arguments' for reader.py

--- 04.underground_chat/reader.py
+++ 04.underground_chat/reader.py
@@ -2,12 +2,21 @@ import argparse
 import asyncio
 import datetime
 import os
+import logging
 
 import aiofiles
 
 SERVER_HOST = 'minechat.dvmn.org'
 SERVER_READ_PORT = 5000
+HISTORY = 'minechat-history.txt'
 
+# python3 listen-minechat.py
+#   --host 192.168.0.1
+#   --port 5001
+#   --history ~/minechat.history
+
+# TODO: 3.Создайте защиту от обрыва соединения
+# TODO: 4.Сохраните историю переписки в файл
 
 # https://www.google.com/search?q=asyncio+tcp+handle+reconnection&oq=asyncio+tcp+handle+reconnection++&aqs=chrome..69i57.23573j0j1&sourceid=chrome&ie=UTF-8
 # https://www.reddit.com/r/learnpython/comments/4a64g4/chat_server_client_asyncio_reconnect_client_if/
@@ -27,12 +36,10 @@ async def connect(server):
             await asyncio.sleep(5)
 
 
-async def tcp_client(server, history):
+async def tcp_client(host, port, history):
     while True:
         try:
-            print('Before connection!')
-            reader, writer = await connect(server)
-            print('Connected!')
+            reader, writer = await connect((host, port))
 
             async with aiofiles.open(history, 'a') as file:
                 while True:
@@ -49,7 +56,44 @@ async def log_to_file(message, file):
     timestamp = now.strftime("%Y.%m.%d %H:%M")
 
     log = f'[{timestamp}] {message}'
-
-    print(log)
     await file.write(log)
-    await file.fsync()
+    logging.info(message)
+
+
+def get_arguments(host, port, history):
+    parser = argparse.ArgumentParser()
+    # TODO: add help strings
+    parser.add_argument('--host', type=str, help='')
+    parser.add_argument('--port', type=str, help='')
+    parser.add_argument('--history', type=str, help='')
+
+    parser.set_defaults(
+        host=host,
+        port=port,
+        history=history,
+    )
+    args = parser.parse_args()
+    return vars(args)
+
+
+def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s: %(message)s',
+        datefmt='%H:%M:%S',
+    )
+
+    args = get_arguments(
+        os.getenv('CHAT_HOST', SERVER_HOST),
+        os.getenv('SERVER_READ_PORT', SERVER_READ_PORT),
+        os.getenv('HISTORY', HISTORY),
+    )
+
+    loop = asyncio.get_event_loop()
+    loop.set_debug(False)
+    loop.run_until_complete(tcp_client(**args))
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
