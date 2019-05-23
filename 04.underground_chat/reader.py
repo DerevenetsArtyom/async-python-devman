1ade063b41aeb6b3c4a25ea0bc99392abd50a47e
derevenets_artem <artem@comvoy.be>
2019-05-23 20:58:45 +0300

    04: Clean up 'reader.py' code, remove useless comments

--- 04.underground_chat/reader.py
+++ 04.underground_chat/reader.py
@@ -8,36 +8,28 @@ import aiofiles
 
 from constants import SERVER_READ_PORT, SERVER_HOST, HISTORY
 
-# python3 listen-minechat.py
-#   --host 192.168.0.1
-#   --port 5001
-#   --history ~/minechat.history
 
-# TODO: 3.Создайте защиту от обрыва соединения
-# TODO: 4.Сохраните историю переписки в файл
-
-# https://www.google.com/search?q=asyncio+tcp+handle+reconnection&oq=asyncio+tcp+handle+reconnection++&aqs=chrome..69i57.23573j0j1&sourceid=chrome&ie=UTF-8
-# https://www.reddit.com/r/learnpython/comments/4a64g4/chat_server_client_asyncio_reconnect_client_if/
-# https://stackoverflow.com/questions/25998394/how-to-reconnect-a-socket-on-asyncio
-# https://github.com/crossbario/autobahn-python/issues/295
-
-
-async def connect(server):
+async def connect(server, history):
     """Set up re-connection for client"""
     while True:
         try:
             reader, writer = await asyncio.open_connection(*server)
+            async with aiofiles.open(history, 'a') as file:
+                await log_to_file('Connection set up!', file)
             return reader, writer
         except (ConnectionRefusedError, ConnectionResetError) as e:
-            print(e)
-            print("Server not up retrying in 5 seconds...")
+            logging.info(e)
+            async with aiofiles.open(history, 'a') as file:
+                await log_to_file(
+                    "Connection error, retrying in 5 seconds...", file)
+            logging.info("Connection error, retrying in 5 seconds...")
             await asyncio.sleep(5)
 
 
 async def tcp_client(host, port, history):
     while True:
         try:
-            reader, writer = await connect((host, port))
+            reader, writer = await connect((host, port), history)
 
             async with aiofiles.open(history, 'a') as file:
                 while True:
@@ -45,7 +37,7 @@ async def tcp_client(host, port, history):
                     await log_to_file(data.decode(), file)
 
         except Exception as e:
-            print(e)
+            logging.info(e)
             continue
 
 
@@ -60,10 +52,9 @@ async def log_to_file(message, file):
 
 def get_arguments(host, port, history):
     parser = argparse.ArgumentParser()
-    # TODO: add help strings
-    parser.add_argument('--host', type=str, help='')
-    parser.add_argument('--port', type=str, help='')
-    parser.add_argument('--history', type=str, help='')
+    parser.add_argument('--host', type=str, help='Host to connect')
+    parser.add_argument('--port', type=str, help='Port to connect')
+    parser.add_argument('--history', type=str, help='Path to history file')
 
     parser.set_defaults(
         host=host,
