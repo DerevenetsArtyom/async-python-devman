7d0e7316343124648e82ed82dba2160e2381ea26
derevenets_artem <artem@comvoy.be>
2019-06-04 21:31:47 +0300

    05: Add a couple of test messages to display

--- 05.underground_chat_client/main.py
+++ 05.underground_chat_client/main.py
@@ -8,6 +8,9 @@ messages_queue = asyncio.Queue()
 sending_queue = asyncio.Queue()
 status_updates_queue = asyncio.Queue()
 
+messages_queue.put_nowait('Иван: Привет всем в этом чатике!')
+messages_queue.put_nowait('Иван: Как дела?')
+
 loop.run_until_complete(
     gui.draw(
         messages_queue,
