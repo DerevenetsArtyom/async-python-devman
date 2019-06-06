ecd4f078b955ebf1c66c642605fc0f0955a6b52d
derevenets_artem <artem@comvoy.be>
2019-06-06 21:27:37 +0300

    05: Add coroutine 'read_messages' to work with remote server

--- 05.underground_chat_client/main.py
+++ 05.underground_chat_client/main.py
@@ -1,6 +1,16 @@
+import argparse
 import asyncio
-import time
+import os
+
+from dotenv import load_dotenv
+
 import gui
+from helpers import connect
+
+SERVER_HOST = 'minechat.dvmn.org'
+SERVER_WRITE_PORT = 5050
+SERVER_READ_PORT = 5000
+
 
 # Программе понадобятся несколько параллельных задач —
 # одна рисует окно интерфейса,
@@ -8,14 +18,22 @@ import gui
 # третья отравляет сообщения.
 
 
-async def generate_messages(queue):
+async def read_messages(host, port, queue):
     while True:
-        message = f'Ping {int(time.time())}'
-        queue.put_nowait(message)
-        await asyncio.sleep(1)
+        try:
+            reader, writer = await connect((host, port))
+
+            while True:
+                data = await reader.readline()
+                queue.put_nowait(data.decode().strip())
+
+        except Exception as e:
+            continue
+        finally:
+            writer.close()
 
 
-async def main():
+async def start(host, port):
     # Queues must be created inside the loop.
     # If create them outside the loop created for asyncio.run(),
     # so they use events.get_event_loop().
@@ -26,8 +44,33 @@ async def main():
     status_updates_queue = asyncio.Queue()
 
     await asyncio.gather(
-        generate_messages(messages_queue),
+        read_messages(host, port, messages_queue),
         gui.draw(messages_queue, sending_queue, status_updates_queue)
     )
 
-asyncio.run(main())
+
+def get_arguments(host, port):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, help='Host to connect')
+    parser.add_argument('--port', type=str, help='Port to connect')
+
+    parser.set_defaults(
+        host=host,
+        port=port,
+    )
+    args = parser.parse_args()
+    return vars(args)
+
+
+def main():
+    load_dotenv()
+    args = get_arguments(
+        os.getenv('CHAT_HOST', SERVER_HOST),
+        os.getenv('SERVER_READ_PORT', SERVER_READ_PORT),
+    )
+
+    asyncio.run(start(**args))
+
+
+if __name__ == '__main__':
+    main()
