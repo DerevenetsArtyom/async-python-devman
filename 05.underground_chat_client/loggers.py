8dc6634960b3f942e13f7a6b55db17415a13acdd
derevenets_artem <artem@comvoy.be>
2019-06-17 19:01:12 +0300

    05: Create 'setup_logger' function

---
+++ 05.underground_chat_client/loggers.py
@@ -0,0 +1,21 @@
+import logging
+
+
+def setup_logger(logger_name, log_file=None, level=logging.INFO):
+    if not log_file:
+        log_file = f'{logger_name}.log'
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+
+    file_handler = logging.FileHandler(log_file)
+    logger.addHandler(file_handler)
+    stream_handler = logging.StreamHandler()
+    logger.addHandler(stream_handler)
+
+    formatter = logging.Formatter(fmt='%(asctime)s: %(message)s',
+                                  datefmt='%H:%M:%S')
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+
+
