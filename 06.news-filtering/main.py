3f705a9ce8af17e76617a6ca9c509facbf37c6ae
derevenets_artem <artem@comvoy.be>
2019-07-30 21:59:08 +0300

    06: Add some links to read about requesting blocked site

--- 06.news-filtering/main.py
+++ 06.news-filtering/main.py
@@ -1,6 +1,14 @@
 import aiohttp
 import asyncio
 
+# Links to check to setup TOR connection:
+# https://jarroba.com/anonymous-scraping-by-tor-network/
+# https://www.sylvaindurand.org/use-tor-with-python/
+# https://stackoverflow.com/questions/30286293/make-requests-using-python-over-tor
+# http://qaru.site/questions/7264187/how-to-use-socks-proxies-to-make-requests-with-aiohttp
+# https://stackoverflow.com/questions/55191914/how-to-connect-to-onion-sites-using-python-aiohttp
+# https://gist.github.com/keepitsimple/1de1306a396d5534d7f3302e606ba72a
+
 
 async def fetch(session, url):
     async with session.get(url) as response:
