676ea8f151c01f628a946433e311b044a0d94b6c
derevenets_artem <artem@comvoy.be>
2019-07-31 21:25:30 +0300

    06: Move articles with information about TOR to separate file

--- 06.news-filtering/main.py
+++ 06.news-filtering/main.py
@@ -1,14 +1,6 @@
-import aiohttp
 import asyncio
 
-# Links to check to setup TOR connection:
-# https://jarroba.com/anonymous-scraping-by-tor-network/
-# https://www.sylvaindurand.org/use-tor-with-python/
-# https://stackoverflow.com/questions/30286293/make-requests-using-python-over-tor
-# http://qaru.site/questions/7264187/how-to-use-socks-proxies-to-make-requests-with-aiohttp
-# https://stackoverflow.com/questions/55191914/how-to-connect-to-onion-sites-using-python-aiohttp
-# https://gist.github.com/keepitsimple/1de1306a396d5534d7f3302e606ba72a
-
+import aiohttp
 from aiohttp_socks import SocksConnector
 
 URL = 'https://inosmi.ru/culture/20190731/245546920.html'
