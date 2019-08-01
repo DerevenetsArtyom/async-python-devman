9251b698125d2c6d20ab074fe5f30e9866dae91c
derevenets_artem <artem@comvoy.be>
2019-08-01 21:39:37 +0300

    06: Add reading article from saved file without requesting every time

--- 06.news-filtering/main.py
+++ 06.news-filtering/main.py
@@ -3,7 +3,11 @@ import asyncio
 import aiohttp
 from aiohttp_socks import SocksConnector
 
+from adapters import SANITIZERS
+
 URL = 'https://inosmi.ru/culture/20190731/245546920.html'
+
+sanitize = SANITIZERS['inosmi_ru']
 
 
 async def fetch(session, url):
@@ -13,10 +17,16 @@ async def fetch(session, url):
 
 
 async def main():
-    connector = SocksConnector.from_url('socks5://127.0.0.1:9050', rdns=True)
-    async with aiohttp.ClientSession(connector=connector) as session:
-        html = await fetch(session, URL)
-        print(html)
+    # connector = SocksConnector.from_url('socks5://127.0.0.1:9050', rdns=True)
+    # async with aiohttp.ClientSession(connector=connector) as session:
+    #     html = await fetch(session, URL)
+    #     clean_plaintext = sanitize(html, plaintext=True)
+    #     print(clean_plaintext)
+
+    with open('text.txt') as f:
+        html = f.read()
+        clean_plaintext = sanitize(html, plaintext=True)
+        print(clean_plaintext)
 
 
 asyncio.run(main())
