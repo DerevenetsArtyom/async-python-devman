86052301dfbfc446d73e446262c78171b9b85681
derevenets_artem <artem@comvoy.be>
2019-08-05 20:19:32 +0300

    06: Add simple rating calculation

--- 06.news-filtering/main.py
+++ 06.news-filtering/main.py
@@ -1,9 +1,12 @@
 import asyncio
 
 import aiohttp
+import pymorphy2
 from aiohttp_socks import SocksConnector
 
 from adapters import SANITIZERS
+from text_tools import split_by_words, calculate_jaundice_rate
+
 
 URL = 'https://inosmi.ru/culture/20190731/245546920.html'
 
@@ -23,10 +26,24 @@ async def main():
     #     clean_plaintext = sanitize(html, plaintext=True)
     #     print(clean_plaintext)
 
+    morph = pymorphy2.MorphAnalyzer()
+
     with open('text.txt') as f:
         html = f.read()
-        clean_plaintext = sanitize(html, plaintext=True)
-        print(clean_plaintext)
+
+    clean_plaintext = sanitize(html, plaintext=True)
+    article_words = split_by_words(morph, clean_plaintext)
+
+    negative_words = [
+        line.rstrip('\n') for line in open('charged_dict/negative_words.txt')
+    ]
+    positive_words = [
+        line.rstrip('\n') for line in open('charged_dict/positive_words.txt')
+    ]
+    full_list = [*negative_words, *positive_words]
+
+    print('Words in article', len(article_words))
+    print('Rating', calculate_jaundice_rate(article_words, full_list))
 
 
 asyncio.run(main())
