363377bfcfd5a0dd2e3b98de6f111561ec4784cd
derevenets_artem <artem@comvoy.be>
2019-10-12 16:02:17 +0300

    07: Start displaying single bus route (4th step)

--- 07.buses-on-the-map/server.py
+++ 07.buses-on-the-map/server.py
@@ -2,24 +2,30 @@ import trio
 from trio_websocket import serve_websocket, ConnectionClosed
 import json
 
-msg = {
+message = {
     "msgType": "Buses",
     "buses": [
-        {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
-        {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
+        {"busId": "c790сс", "lat": None, "lng": None, "route": "120"},
     ]
 }
+
+with open("routes/156.json") as f:
+    bus_info = json.loads(f.read())
 
 
 async def echo_server(request):
     ws = await request.accept()
-    # while True:
-    try:
-        message = json.dumps(msg)
-        await ws.send_message(message)
-    except ConnectionClosed:
-        pass
-        # break
+    while True:
+        for coordinates in bus_info["coordinates"]:
+            try:
+                message["buses"][0]["lat"] = coordinates[0]
+                message["buses"][0]["lng"] = coordinates[1]
+
+                await ws.send_message(json.dumps(message))
+
+                await trio.sleep(0.5)
+            except ConnectionClosed:
+                break
 
 
 async def main():
