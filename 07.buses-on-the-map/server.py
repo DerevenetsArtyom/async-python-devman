03697d8df9f0a61e155d334fb1a360be26185dab
derevenets_artem <artem@comvoy.be>
2019-10-12 15:30:08 +0300

    07: Add sending dummy bus coordinates (3-rd step)

--- 07.buses-on-the-map/server.py
+++ 07.buses-on-the-map/server.py
@@ -1,15 +1,25 @@
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed
+import json
+
+msg = {
+    "msgType": "Buses",
+    "buses": [
+        {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
+        {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
+    ]
+}
 
 
 async def echo_server(request):
     ws = await request.accept()
-    while True:
-        try:
-            message = await ws.get_message()
-            await ws.send_message(message)
-        except ConnectionClosed:
-            break
+    # while True:
+    try:
+        message = json.dumps(msg)
+        await ws.send_message(message)
+    except ConnectionClosed:
+        pass
+        # break
 
 
 async def main():
