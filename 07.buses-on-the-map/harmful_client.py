d2e73d7c9d00359f607f8fbeaf3606896ffc1ba8
derevenets_artem <artem@comvoy.be>
2019-12-05 23:11:35 +0200

    07: Create 'harmful_client'

---
+++ 07.buses-on-the-map/harmful_client.py
@@ -0,0 +1,36 @@
+import json
+import random
+import contextlib
+
+
+import trio
+from trio_websocket import open_websocket_url, ConnectionClosed
+
+
+ERROR_MESSAGES = [
+    json.dumps({"data": {'east_lng': 20, 'north_lat': 20, 'south_lat': 20, 'west_lng': 35}}),
+    json.dumps({'msgType': 'Error Msg Type'}),
+    json.dumps({'msgType': 'newBounds', "data": {'lat': 20, 'north_lat': 30}}),
+]
+
+
+async def main():
+    url = 'ws://127.0.0.1:8000'
+    async with open_websocket_url(url) as ws:
+        while True:
+            try:
+                error_message = random.choice(ERROR_MESSAGES)
+                await ws.send_message(error_message)
+                print(f'Sent message "{error_message}" to {url}')
+
+                message = await ws.get_message()
+                print(f'Received message: {message}')
+            except (OSError, ConnectionClosed) as e:
+                print(f'Connection failed: {e}')
+                break
+            await trio.sleep(1)
+
+
+with contextlib.suppress(KeyboardInterrupt):
+    trio.run(main)
+
