099b8cbf1ed36bebb279ada1656247fd06c49cf1
derevenets_artem <artem@comvoy.be>
2019-12-05 23:08:08 +0200

    07: Create schema 'WindowBoundsSchema' for message validation

---
+++ 07.buses-on-the-map/schema.py
@@ -0,0 +1,16 @@
+from marshmallow import Schema, fields, validate
+
+
+class WindowBoundsSchema(Schema):
+    msgType = fields.String(
+        required=True, validate=validate.OneOf(["newBounds", ])
+    )
+    data = fields.Nested("WindowBoundsDataSchema", required=True)
+
+
+class WindowBoundsDataSchema(Schema):
+    south_lat = fields.Float(required=True)
+    north_lat = fields.Float(required=True)
+    west_lng = fields.Float(required=True)
+    east_lng = fields.Float(required=True)
+
