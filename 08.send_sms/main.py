937ca384218cff442861db2e9246dd7003d099ea
derevenets_artem <artem@comvoy.be>
2020-03-11 17:57:53 +0200

    08: Create template for 'request_smsc' function

---
+++ 08.send_sms/main.py
@@ -0,0 +1,24 @@
+import asks
+import trio
+
+
+async def request_smsc(method, login, password, payload):
+    """Send request to SMSC.ru service.
+
+    Args:
+        method (str): API method. E.g. 'send' or 'status'.
+        login (str): Login for account on SMSC.
+        password (str): Password for account on SMSC.
+        payload (dict): Additional request params, override default ones.
+    Returns:
+        dict: Response from SMSC API.
+    Raises:
+        SmscApiError: If SMSC API response status is not 200 or it has `"ERROR" in response.
+
+    Examples:
+        >>> request_smsc("send", "my_login", "my_password", {"phones": "+79123456789"})
+        {"cnt": 1, "id": 24}
+        >>> request_smsc("status", "my_login", "my_password", {"phone": "+79123456789", "id": "24"})
+        {'status': 1, 'last_date': '28.12.2019 19:20:22', 'last_timestamp': 1577550022}
+    """
+    pass
