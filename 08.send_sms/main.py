38cd12df3ffcfdb41611414011969d2039a5395a
derevenets_artem <artem@comvoy.be>
2020-03-11 19:37:11 +0200

    08: Add error handling and logic for 'send' method in 'request_smsc' function

--- 08.send_sms/main.py
+++ 08.send_sms/main.py
@@ -4,6 +4,8 @@ import os
 
 from dotenv import load_dotenv
 
+from exceptions import SmscApiError
+
 
 async def request_smsc(method, login, password, payload):
     """Send request to SMSC.ru service.
@@ -24,7 +26,45 @@ async def request_smsc(method, login, password, payload):
         >>> request_smsc("status", "my_login", "my_password", {"phone": "+79123456789", "id": "24"})
         {'status': 1, 'last_date': '28.12.2019 19:20:22', 'last_timestamp': 1577550022}
     """
-    pass
+
+    if not payload:
+        raise SmscApiError("Payload is not passed")
+
+    key_is_present = False
+    for key in payload:
+        if key == "phones" or key == "phone":
+            key_is_present = True
+    if not key_is_present:
+        raise SmscApiError("Payload should contain 'phone' or 'phones' keys")
+
+    common_url = f"https://smsc.ru/sys/{method}.php?login={login}&psw={password}"
+    result = None
+
+    if method == "send":
+        payload_str = "".join([f"{key}={value}" for key, value in payload.items()])
+        message = "Внимание,%20вечером%20будет%20шторм!"
+        formatting = 3
+        cost = 3
+        charset = "utf-8"
+
+        url = f"{common_url}&{payload_str}&mes={message}&charset={charset}&fmt={formatting}&cost={cost}"
+        response = await asks.get(url)
+        # {"id": 63, "cnt": 1, "cost": "3.8", "balance": "286.55"}
+        # {'id': 67, 'cnt': 1, 'cost': '3.8', 'balance': '282.75'}
+
+        result = response.json()
+
+    elif method == "status":
+        pass
+    else:
+        raise SmscApiError("Unsupported method")
+
+    print('result', result)
+
+    if "error" in result:
+        #  {'error': 'invalid number', 'error_code': 7, 'id': 64}
+        #  {"error": "duplicate request, wait a minute", "error_code": 9}
+        raise SmscApiError(f"An error occurred during request: '{result['error']}' with code {result['error_code']}")
 
 
 async def main():
@@ -32,8 +72,9 @@ async def main():
 
     login = os.getenv("LOGIN")
     password = os.getenv("PASSWORD")
+    phone = os.getenv("PHONE")
 
-    await request_smsc('method', login, password, {})
+    await request_smsc('send', login, password, {"phones": phone})
 
 
 if __name__ == "__main__":
