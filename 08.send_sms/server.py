ef81cb3d4cc967a09e34d455bea6839af9dca17d
derevenets_artem <artem@comvoy.be>
2020-03-12 20:53:22 +0200

    08: Add basic handler for '/send/' route

--- 08.send_sms/server.py
+++ 08.send_sms/server.py
@@ -1,4 +1,4 @@
-from quart import websocket, render_template
+from quart import websocket, render_template, request
 
 from quart_trio import QuartTrio
 
@@ -15,6 +15,16 @@ async def hello():
         "percentFailed": "11",
     }
     return await render_template('index.html', mailing=context)
+
+
+@app.route('/send/', methods=['POST'])
+async def send():
+    # data = await request.get_data()  # Full raw body
+    # print('data', data)
+    form = await request.form
+    text = form["text"]
+    print('text', text)
+    return {}
 
 
 @app.websocket('/ws')
