7e27993c707de5eb1ed479d276b56f7f897a818a
derevenets_artem <artem@comvoy.be>
2020-03-14 11:39:59 +0200

    08: Wrap asyncio calls into 'trio_asyncio.run_asyncio()' in 'trio_db_example.py'

--- 08.send_sms/db/trio_db_example.py
+++ 08.send_sms/db/trio_db_example.py
@@ -1,17 +1,23 @@
+import functools
 import asyncio
+import warnings
+
 import aioredis
 import argparse
+
+import trio
+from trio import TrioDeprecationWarning
 
 import trio_asyncio
 
 from db import Database
 
+warnings.filterwarnings("ignore", category=TrioDeprecationWarning)
+
 
 def create_argparser():
     parser = argparse.ArgumentParser(description='Redis database usage example')
-    parser.add_argument('--address', action='store', dest='redis_uri', help='Redis URI')
-    parser.add_argument('--password', action='store', dest='redis_password', help='Redis db password')
-
+    parser.add_argument('--address', action='store', dest='redis_uri', default="redis://127.0.0.1:6379")
     return parser
 
 
@@ -23,14 +29,11 @@ async def main():
     parser = create_argparser()
     args = parser.parse_args()
 
-    redis = await aioredis.create_redis_pool(
-        args.redis_uri,
-        password=args.redis_password,
-        encoding='utf-8',
-    )
+    # WAS BEFORE: redis = await aioredis.create_redis_pool(args.redis_uri, encoding='utf-8')
+    create_redis_pool = functools.partial(aioredis.create_redis_pool, encoding='utf-8')
+    redis = await trio_asyncio.run_asyncio(create_redis_pool, args.redis_uri)
 
     try:
-
         db = Database(redis)
 
         sms_id = '1'
@@ -42,16 +45,20 @@ async def main():
         ]
         text = 'Вечером будет шторм!'
 
-        await db.add_sms_mailing(sms_id, phones, text)
+        # WAS BEFORE: await db.add_sms_mailing(...)
+        await trio_asyncio.run_asyncio(db.add_sms_mailing, sms_id, phones, text)
 
-        sms_ids = await db.list_sms_mailings()
+        # WAS BEFORE: sms_ids = await db.list_sms_mailings()
+        sms_ids = await trio_asyncio.run_asyncio(db.list_sms_mailings)
         print('Registered mailings ids', sms_ids)
 
-        pending_sms_list = await db.get_pending_sms_list()
+        # WAS BEFORE: pending_sms_list = await db.get_pending_sms_list()
+        pending_sms_list = await trio_asyncio.run_asyncio(db.get_pending_sms_list)
         print('pending:')
         print(pending_sms_list)
 
-        await db.update_sms_status_in_bulk([
+        # WAS BEFORE: await db.update_sms_status_in_bulk([ ... ])
+        await trio_asyncio.run_asyncio(db.update_sms_status_in_bulk, [
             # [sms_id, phone_number, status]
             [sms_id, '112', 'failed'],
             [sms_id, '911', 'pending'],
@@ -59,24 +66,31 @@ async def main():
             # following statuses are available: failed, pending, delivered
         ])
 
-        pending_sms_list = await db.get_pending_sms_list()
+        # WAS BEFORE: pending_sms_list = await db.get_pending_sms_list()
+        pending_sms_list = await trio_asyncio.run_asyncio(db.get_pending_sms_list)
         print('pending:')
         print(pending_sms_list)
 
-        sms_mailings = await db.get_sms_mailings('1')
+        # WAS BEFORE: sms_mailings = await db.get_sms_mailings('1')
+        sms_mailings = await trio_asyncio.run_asyncio(db.get_sms_mailings, '1')
         print('sms_mailings')
         print(sms_mailings)
 
         async def send():
             while True:
-                await asyncio.sleep(1)
-                await redis.publish('updates', sms_id)
+                # WAS BEFORE: await asyncio.sleep(1)
+                await trio.sleep(1)
+
+                # WAS BEFORE: await redis.publish('updates', sms_id)
+                await trio_asyncio.run_asyncio(redis.publish, 'updates', sms_id)
 
         async def listen():
-            *_, channel = await redis.subscribe('updates')
+            # WAS BEFORE: *_, channel = await redis.subscribe('updates')
+            *_, channel = await trio_asyncio.run_asyncio(redis.subscribe, 'updates')
 
             while True:
-                raw_message = await channel.get()
+                # WAS BEFORE: raw_message = await channel.get()
+                raw_message = await trio_asyncio.run_asyncio(channel.get)
 
                 if not raw_message:
                     raise ConnectionError('Connection was lost')
@@ -84,17 +98,16 @@ async def main():
                 message = raw_message.decode('utf-8')
                 print('Got message:', message)
 
-        await asyncio.gather(
-            send(),
-            listen()
-        )
+        # WAS BEFORE: await asyncio.gather(send(), listen())
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(send)
+            nursery.start_soon(listen)
 
     finally:
         redis.close()
-        await redis.wait_closed()
+        # WAS BEFORE: await redis.wait_closed()
+        await trio_asyncio.run_asyncio(redis.wait_closed)
 
 
 if __name__ == '__main__':
-    trio_asyncio.run(
-        trio_asyncio.aio_as_trio(main)
-    )
+    trio_asyncio.run(main)
